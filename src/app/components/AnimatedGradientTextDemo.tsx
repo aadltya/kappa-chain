9dd1168734b2ff781e9e442bc54d974c9490bcad
aadltya <adityaedu22@gmail.com>
2025-02-04 23:39:29 +0530

    Y combinator

---
+++ src/app/components/AnimatedGradientTextDemo.tsx
@@ -0,0 +1,31 @@
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { AnimatedGradientText } from "./ui/animated-gradient-text";
+
+export function AnimatedGradientTextDemo() {
+  return (
+    <div className="z-10 absolute flex min-h-64 items-center justify-center">
+      <AnimatedGradientText>
+        <span
+          className={cn(
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+          )}
+        >
+          Backed by
+        </span>
+        <img 
+        src="https://www.vectorlogo.zone/logos/ycombinator/ycombinator-tile.svg" 
+        width={25} 
+        height={25}  
+        alt="Y" 
+        className="pl-2"
+        />
+
+        <span className="text-orange-500 pl-2" >Combinator (W25)</span>
+
+        {/* <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "} */}
+        <ChevronRight className="ml-1 text-white size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+      </AnimatedGradientText>
+    </div>
+  );
+}
--- src/app/components/SpotlightNew.tsx
+++ src/app/components/SpotlightNew.tsx
@@ -1,19 +1,32 @@
 "use client";
+
 import React from "react";
 import { Spotlight } from "./spotlight-new";
+import { TextRevealCardPreview } from "./TextRevealCardPreview";
+import { AnimatedGradientTextDemo } from "./AnimatedGradientTextDemo";
+import { AnimatedShinyTextDemo } from "./AnimatedShinyTextDemo";
 
 export function SpotlightNew() {
   return (
-    <div className="h-screen w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Spotlight <br /> which is not overused.
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          A subtle yet effective spotlight effect, because the previous version
-          is used a bit too much these days.
-        </p>
+    <div className="h=screen w-full flex justify-center" >
+      <AnimatedGradientTextDemo />
+      <div className="h-screen w-full flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <Spotlight />
+        <div className="p-4 mx-auto relative z-10 w-full pt-20 px-10 md:pt-0 flex justify-between">
+          <div className="w-1/2">
+            <h1 className="text-4xl md:text-6xl font-bold text-start bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              Stablecoin for <br /> Global Payments.
+            </h1>
+            <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-start">
+            Power instant global transfers with a single integration.
+            Grow more, pay less, move faster.
+            </p>
+            <AnimatedShinyTextDemo />
+          </div>
+          <div className="w-1/2 flex items-center justify-center">
+            <TextRevealCardPreview />
+          </div>
+        </div>
       </div>
     </div>
   );
